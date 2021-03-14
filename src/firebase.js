ef7ac4724149a22599a1422865c7ad44771500f1
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2021-03-14 08:41:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdUjceXMONPBPCd_NX_4fNChVenNZ_CmQ",
-  authDomain: "slack-clone-9eabf.firebaseapp.com",
-  projectId: "slack-clone-9eabf",
-  storageBucket: "slack-clone-9eabf.appspot.com",
-  messagingSenderId: "637942742117",
-  appId: "1:637942742117:web:987449721d36268b4c1a8c",
-  measurementId: "G-9WPZKXQ7RG",
+ //copy your cdn config here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
